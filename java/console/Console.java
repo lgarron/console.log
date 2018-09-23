693a32a818131271ae22b3a2a3725d97f650a02f
Ajit Singh <ajit.singh2905@gmail.com>
2018-09-23 20:55:08 +0530

    add Java Console.log

---
+++ java/console/Console.java
@@ -0,0 +1,19 @@
+package console;
+
+import java.util.Arrays;
+
+public class Console {
+	public static void log(Object... args) {
+		for(int i = 0; i < args.length; ++i) {
+			if(args[i].getClass().isArray()) {
+				Object[] arr = {args[i]};
+				String arrayString = Arrays.deepToString(arr);
+				arrayString = arrayString.substring(1, arrayString.length()-1);
+				System.out.print(arrayString + " ");
+			}
+			else
+				System.out.print(args[i].toString() + " ");
+		}
+		System.out.println();
+	}
+}
---
+++ java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import console.Console;
+
+public class Test {
+    public static void main(String[] args) {
+    	int[][] arr = {{1,2,3}, {4,5}};
+    	String s = "Can print arrays too:";
+    	Console.log(s, arr);
+    }
+}
