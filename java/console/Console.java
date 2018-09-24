37a363a5b5d3cc0ee6dc74de5279318f1a583d23
Ajit Singh <ajit.singh2905@gmail.com>
2018-09-25 02:37:52 +0530

    converting `console` case

--- java/console/Console.java
+++ java/console/Console.java
@@ -2,7 +2,7 @@ package console;
 
 import java.util.Arrays;
 
-public class Console {
+public class console {
 	public static void log(Object... args) {
 		for(int i = 0; i < args.length; ++i) {
 			if(args[i].getClass().isArray()) {
